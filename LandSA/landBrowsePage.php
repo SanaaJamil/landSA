26af4e97d285dff8460b5d72b1d07813f1018e44
SanaaJamil <85866589+SanaaJamil@users.noreply.github.com>
2022-01-30 21:26:48 +0300

    Update landBrowsePage.php

--- LandSA/landBrowsePage.php
+++ LandSA/landBrowsePage.php
@@ -20,11 +20,30 @@
 			<!-- send the search attribute -->
 			<button><input type="submit" value="إبحث" ></button>
 		</form>
-		</div>
-		<form>
-			<div id="nationality" w3-include-html="components/nationality.html"></div>
-			<button><input type="submit" value="submit" ></button>
-		</form>
+	</div>
+
+	<br>
+	<div class="topnav"> <!-- land A browse
+	<div id="nationality" w3-include-html="components/nationality.html"> </div> !-->
+		<p style="position: relative;left:-30px;display: inline; ">
+			<img src="images/Riyadh.jpg">
+		</p>
+		<h4> 
+			<div style="float:left; display: inline;">Left Text</div>
+			<div style="clear: left;"/>
+		</h4>
+		<br> <!-- only regesterd cans viwe the details -->
+		<button style="position: relative;right: 500px;"><input type="submit" value="عرض التفاصيل " ></button>
+
+
+	</div>
+
+	<br>
+	<div class="topnav">
+		<button style="position: relative;right: 1000px;"><input type="submit" value="عرض التفاصيل " ></button>
+
+
+	</div>
 		
 		<!-- footer -->
 	</div>
