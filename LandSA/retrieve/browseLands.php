5d0cbc8e26166f3eb74cecd8fdd0fe71068f1b16
SanaaJamil <85866589+SanaaJamil@users.noreply.github.com>
2022-01-31 18:21:35 +0300

    Update browseLands.php

--- LandSA/retrieve/browseLands.php
+++ LandSA/retrieve/browseLands.php
@@ -15,7 +15,7 @@ $db = mysqli_select_db($con,"landsa") or die("Error Can't Connect to DB");
 $sql = "SELECT neighborhoodName, address, city FROM landrecord WHERE landState='yes'";
 $result = $con->query($sql);
 $num_rows = mysqli_num_rows($result);
-echo "<a href='../landBrowsePage.php'>الرجوع للخلف </a>". "<br>". "<br>". "<br>";
+echo "<a href='../landBrowsePage.php'>عذرا, قم بتسجيل الدخول</a>". "<br>". "<br>". "<br>";
 //to print number of rows:
 //echo "$num_rows Rows\n";
 
@@ -25,11 +25,11 @@ if ($result->num_rows > 0) {
     echo "city: " . $row["city"]. " - neighborhoodName: " . $row["neighborhoodName"]. " " . $row["address"]. "<br>";
   }
 } else {//if no results
-  echo"      ||     ";
-  echo "عذرا, لايمكنك رؤية التفاصيل لأنك غير مسجل </a>     ||     ". "<br>";
-  echo "<a href='../sign_log/login.html
-'>تسجيل الدخول </a>". "<br>";
-  echo "<a href='../sign_log/singUp.html'>مستخدم جديد</a>". "<br>";
+
+//echo "عذرا, لايمكنك رؤية التفاصي
+//echo"      ||     ";ل لأنك غير مسجل </a>     ||     ". "<br>";
+//echo "<a href='../sign_log/login.html'>تسجيل الدخول </a>". "<br>";
+//echo "<a href='../sign_log/singUp.html'>مستخدم جديد</a>". "<br>";
 
 }
 $con->close();
