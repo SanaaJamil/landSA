1e4f70b4353ff30e25491a67d14ae75d6d3386ec
SanaaJamil <85866589+SanaaJamil@users.noreply.github.com>
2022-01-31 17:37:49 +0300

    Update browseLands.php

--- LandSA/retrieve/browseLands.php
+++ LandSA/retrieve/browseLands.php
@@ -14,14 +14,23 @@ $db = mysqli_select_db($con,"landsa") or die("Error Can't Connect to DB");
 //select coloumns to be printed on land browse page according to land state
 $sql = "SELECT neighborhoodName, address, city FROM landrecord WHERE landState='yes'";
 $result = $con->query($sql);
+$num_rows = mysqli_num_rows($result);
+echo "<a href='../landBrowsePage.php'>الرجوع للخلف </a>". "<br>". "<br>". "<br>";
+//to print number of rows:
+//echo "$num_rows Rows\n";
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo "city: " . $row["city"]. " - neighborhoodName: " . $row["neighborhoodName"]. " " . $row["address"]. "<br>";
   }
-} else {//if no results 
-  echo "0 results";
+} else {//if no results
+  echo"      ||     ";
+  echo "عذرا, لايمكنك رؤية التفاصيل لأنك غير مسجل </a>     ||     ". "<br>";
+  echo "<a href='../sign_log/login.html
+'>تسجيل الدخول </a>". "<br>";
+  echo "<a href='../sign_log/singUp.html'>مستخدم جديد</a>". "<br>";
+
 }
 $con->close();
 ?>
