7371aca8f91d1b5b960472ef3b27f9174cb0e6b7
SanaaJamil <85866589+SanaaJamil@users.noreply.github.com>
2022-02-06 21:30:13 +0300

    Update landinheritanceFormConnection.php

--- LandSA/landinheritanceFormConnection.php
+++ LandSA/landinheritanceFormConnection.php
@@ -21,18 +21,17 @@
 $courtOrder = $_POST["courtOrder"];
 $OwnerID = $_POST["OwnerID"];
 $REUN = $_POST["REUN"];
-
+$requestID = rand (1000, 9999);
 
 //connect and check connection
 $con = mysqli_connect("localhost","root","","landsa") or die("Error: Can't Connect to Server");
 $db = mysqli_select_db($con,"landsa") or die("Error: Can't Connect to DB");
 
-if(isset($_POST["submit"])){
-        $stmt=$con->prepare("INSERT INTO inheritancerecord (courtOrder,OwnerID,REUN) VALUES (?,?,?)");
-        $stmt -> bind_param("iii",$courtOrder,$OwnerID,$REUN);
+if(($_POST["submit"])){
+        $stmt=$con->prepare("INSERT INTO inheritancerecord (courtOrder,OwnerID,REUN,requestID,UserID) VALUES (?,?,?,?,'2')");
+        $stmt -> bind_param("iiii",$courtOrder,$OwnerID,$REUN,$requestID);
         $stmt->execute();
         echo "Saved successfully";
         echo "<br/>";
         }
-
-?>     
+?>
