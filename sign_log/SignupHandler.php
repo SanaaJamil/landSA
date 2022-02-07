b14d7e59d23c6115d7d7f0b2c665a0945b31a92a
AsmaaAR <86467728+AsmaaAR@users.noreply.github.com>
2022-02-07 15:04:37 +0300

    Update SignupHandler.php

--- sign_log/SignupHandler.php
+++ sign_log/SignupHandler.php
@@ -1,4 +1,7 @@
 <?php
+
+include "../connection.php";
+if (isset($_POST['Submit']))
 $ID = $_POST["ID"];
 $IDType = $_POST["IDType"];
 $fristName = $_POST["fristName"];
@@ -10,21 +13,25 @@ $phoneNum = $_POST["phoneNum"];
 $Email = $_POST["Email"];
 $Password = $_POST["Password"];
 $UserType = 0;
+$password_confirm = $_POST['password_confirm'];
 
-include "../connection.php";
+$select = mysqli_query($con, "SELECT * FROM users WHERE ID = '".$_POST['ID']."'");
+if(mysqli_num_rows($select)) {
+	echo "<script>alert('لديك حساب مسبقا,قم بتسجيل الدخول')</script>";
+    echo "<script>setTimeout(\"location.href = 'Login.php';\",0);</script>";
 
-$insertUser = "INSERT INTO users(ID,IDType,fristName,middleName,lastName,Password,phoneNum,Email,IBAN,BirthDate,UserType) 
-value('$ID','$IDType','$fristName','$middleName','$lastName','$Password','$phoneNum','$Email','$IBAN','$BirthDate','$UserType')";
-$result = mysqli_query($con,$insertUser); #send query to the databaes to use insert method
+}
+else{
+          $insertUser = "INSERT INTO users(ID,IDType,fristName,middleName,lastName,Password,phoneNum,Email,IBAN,BirthDate,UserType) 
+          value('$ID','$IDType','$fristName','$middleName','$lastName','$Password','$phoneNum','$Email','$IBAN','$BirthDate','$UserType')";
+           $result = mysqli_query($con,$insertUser); #send query to the databaes to use insert method
 
-echo "ass ";
-if($result){
-
-	 echo "<h1>User created Successfully</h1>";
+          if($result){
      header('Location: login.php');
-}
-else {
-	die("Error: ".mysqli_errno($con));
-}
+	}
+     else {
+	die("Error: ".mysqli_errno($con)); }
+  	}
+
 
 ?>
