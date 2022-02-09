e986e10008f474d6378bc2a80a2d47da99c7aee3
Taif Alghamdi <62331821+tofagmd@users.noreply.github.com>
2022-02-10 00:51:34 +0300

    Update login.php
    no more confusing codes
    the same style as register land

--- log/login.php
+++ log/login.php
@@ -5,123 +5,64 @@
     <!-- Required meta tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Colorlib Templates">
-    <meta name="author" content="Colorlib">
-    <meta name="keywords" content="Colorlib Templates">
 
     <!-- Title Page-->
     <title>Log in</title>
 
-    <!-- Icons font CSS-->
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <!-- Font special for pages-->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Vendor CSS-->
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
-
-    <!-- Main CSS-->
-    <link href="css/main.css" rel="stylesheet" media="all">
-    <style>*{font-family: "Montserrat", sans-serif;}</style>
-
-    <!-- CSS&JS of header and footer -->
-    <script src="../landSA/components/ComponentHandler.js" ></script>
+    <!--CSS-->
+    <link href="../LandSA/style.css" rel="stylesheet" media="all">
     <style>
-        /*header css*/
-        li, a, button {
-            font-weight: 500; /*from the link on top of the bage*/
-            font-size: 16px;
-            color: #000000;
-            text-decoration: none;
-        }
-
-        .header {
-            background-color: #ffffff;
-            display: flex;
-            justify-content: flex-start; /*push every thing to the right*/
-            align-items: center;
-            padding: 30px 10%; /*30 from top and bottom, 10 from the sides*/
-        }
-        .header {
-            display: flex;
-            flex-flow: row wrap;
-            width: 100%;
-            /* justify-content: flex-end;
-            align-items: center;
-            padding: 30px 10%; 30 from top and bottom, 10 from the sides */
-        }
-        /*each indivital link*/
-        .nav_links Li {
-            display: inline-block; /*view side by side*/
-            padding: 0px 20px;
-        }
-
-        .nav_links li a {
+        button[type=reset] {
+            margin-left: 10px;
+            padding: 9px 25px;
+            background-color: rgba(225, 225, 225, 1); /*used rgb to control the opacity*/
+            border: none;
+            border-radius: 50px;
+            cursor: pointer;
             transition: all 0.3s ease 0s;
         }
-
-        .nav_links li a:hover {
-            color: #0088a9
+        button:hover[type=reset]{
+            background-color: rgba(200, 225, 225, 1);
         }
 
     </style>
+
+    <!-- CSS&JS of header and footer -->
+    <script src="../landSA/components/ComponentHandler.js" ></script>
 </head>
 
 <body>
     <!--Page header-->
 	<div id="Head" w3-include-html="../landSA/components/nav.php"></div>
 
-    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
-        <div class="wrapper wrapper--w680">
-            <div class="card card-4">
-                <div class="card-body">
-                    <img class="logo " src="../LandSA/images/logo.gif" alt="logo" width="300px" style="margin-right: 125px">
-                    <h4 class="title">تسجيل الدخول</h4>
-                    <form method="POST" action="‏‏LoginHandler.php">
-                        <div class="row row-space">
-                            <div class="col-1">
-                                <div class="input-group">
-                                    <label class="label">رقم الهوية</label>
-                                    <input class="input--style-4" type="text" name="ID" style="width: 400px"  maxlength="10" pattern="[0-9]{10}" required>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
 
-                            <div class="col-1">
-                                <div class="input-group">
-                                    <label class="label">كلمة المرور</label>
-                                    <input class="input--style-4" type="Password" name="Password" style="width: 400px" required>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        
-                        <div class="p-t-15">
-                            <button class="btn btn--radius-2 btn--blue" type="submit">ارسال</button>
-                            <button class="btn btn--radius-2 btn--blue" type="submit">إلغاء</button>
-                            
+    <main>
+        <aside></aside>
+        <div class="container" style="width: 650px;">
+            <img class="logo " src="../LandSA/images/logo.gif" alt="logo" width="300px" style="margin-right: 25%;">
+            <h1 class="title">تسجيل الدخول</h1>
 
-                        </div>
-                        <a href="signUp.php">تسجيل الدخول</a>
-                    </form>
+            <form method="POST" action="‏‏LoginHandler.php">
+                <p>رقم الهوية</p>
+                <div class="form">
+                    <input type="text" name="ID" required>
                 </div>
-            </div>
+
+                <p>كلمة المرور</p>
+                <div class="form">
+                    <input type="Password" name="Password" required>
+                </div>
+                
+                
+                <div class="btn">
+                    <button type="reset">إلغاء</button>
+                    <button type="submit">ارسال</button>
+                </div>
+                <a href="signUp.php">تسجيل الدخول</a>
+            </form>
         </div>
-    </div>
-    
-
-    <!-- Jquery JS-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <!-- Vendor JS-->
-    <script src="vendor/select2/select2.min.js"></script>
-    <script src="vendor/datepicker/moment.min.js"></script>
-    <script src="vendor/datepicker/daterangepicker.js"></script>
-
-    <!-- Main JS-->
-    <script src="js/global.js"></script>
+        <aside></aside>
+    </main>
 
     <!-- footer -->
 		<div w3-include-html="../landSA/components/footer.php"></div>
@@ -130,7 +71,7 @@
     includeHTML();
     </script>
 
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 
 </html>
 <!-- end document-->
