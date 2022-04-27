57bf31f00ea1518a1044e8a2df7bf32eb48b50b8
Taif Alghamdi <62331821+tofagmd@users.noreply.github.com>
2022-04-27 18:25:33 +0300

    Add files via upload
    the maps files for saving and for vewing

--- LandSA/map.php
+++ LandSA/map.php
@@ -1,3 +1,23 @@
+<?php
+  include "connection.php";
+  
+
+  if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    // $REUN = $_GET['REUN'];
+
+    echo "hi";
+    $L1 = $_POST['lats'];
+    $L2 = $_POST['longs'];
+    $REUN = 1111;
+
+    $insertLocation = "INSERT INTO `map` (`REUN`, `latitude`, `longitude`) VALUES ('$REUN', '$L1', '$L2');";
+    $query = mysqli_query($con, $insertLocation);
+
+}
+?>
+
+<!-- -------------------------------------------------#HTML Code#-------------------------------------------------- -->
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,35 +30,89 @@
         /* The height is 400 pixels */
         width: 100%;
         /* The width is the width of the web page */
+        background-color: rgba(103,178,147, 0.8);
       }
     </style>
-    <script>
-      // Initialize and add the map
+    <script>      // Initialize and add the map
+      // In the following example, markers appear when the user clicks on the map.
+      var marker;
+      var longitude;
+      var latitude;
+      var map;
+
+
       function initMap() {
-        // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.036 };
-        // The map, centered at Uluru
-        const map = new google.maps.Map(document.getElementById("map"), {
+        var uluru = { lat: 24.7136, lng: 46.6753 };
+        map = new google.maps.Map(document.getElementById("map"), {
           zoom: 12,
           center: uluru,
         });
-        // The marker, positioned at Uluru
-        const marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
+
+        function placeMarker(location) {
+          if ( marker ) {
+            marker.setPosition(location);
+          } else {
+            marker = new google.maps.Marker({
+              position: location,
+              map: map
+            });
+          }
+        }
+
+        google.maps.event.addListener(map, 'click', function(event) {
+          // function (){
+          // let lat= marker.position.lat()
+          // let Ing= marker.position.lng()
+          // $('#latitude').val(lat)
+          // $('#longitude').val(lng)
+          // })
+
+          
+          placeMarker(event.latLng);
+
+          //convert "event.latLng" into string
+          var location = JSON.stringify(event.latLng);
+          //remove unwanted parts of the string to be left with the latitude and longitude
+          newStr = location.replace("{\"lat\":", "");
+          newStr = newStr.replace("\"lng\":", "");
+          newStr = newStr.replace("}", "");
+          //split the string to get an array of the latitude and longitude
+          const myArray = newStr.split(",");
+
+          //save the latitude and longitude in a variable to send to the database
+          latitude = myArray[0];
+          longitude = myArray[1];
+          
+          document.getElementById('latitude').value = latitude;
+          document.getElementById('longitude').value = longitude;
+
+          // document.getElementById('latitude').value = latitude;
+          // document.getElementById('longitude').value = longitude;
         });
       }
+      
     </script>
+    
   </head>
+  
   <body>
     <h3>My Google Maps Demo</h3>
     <!--The div element for the map -->
     <div id="map"></div>
 
+    <!-- <p id="latitude"></p>
+    <p id="longitude"></p> -->
+    
+    <form method="POST">
+      <input  name="lats" id="latitude" />
+      <input  name="longs" id="longitude" />
+      <input type="submit" value="تأكيد الموقع">
+    </form>
+
+
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl39nJCT9GvsrbmIlEexdz9LPr7v_9s3E&callback=initMap&libraries=&v=weekly"
-      async
-    ></script>
+    <script async
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl39nJCT9GvsrbmIlEexdz9LPr7v_9s3E&callback=initMap">
+    </script>
   </body>
 </html>
